0e755e0ee00500d31b425c17043b664a39fb08c9
Eve <evelinheks@gmail.com>
2022-10-27 15:44:53 -0300

    IMC

--- IMC/src/Calculo/TestaIMC.java
+++ IMC/src/Calculo/TestaIMC.java
@@ -1,12 +1,23 @@
 package Calculo;
 
+import java.util.Scanner;
+
 public class TestaIMC {
 	public static void main(String[] args) {
-		Pessoa p1 = new Pessoa();
-		p1.peso = 80;
-		p1.altura = 1.50;
+		int resposta = 0;
+		Scanner entrada = new Scanner(System.in);
+		do {
+			Pessoa p1 = new Pessoa();
+			System.out.println("Digite o peso:");
+			p1.peso = entrada.nextDouble();
+			System.out.println("Digite agora a altura:");
+			p1.altura = entrada.nextDouble();
+			System.out.println(p1.calculoImc());
+			System.out.println("Digite 1 para fazer um novo calculo, ou 0 para encerrar!");
+			resposta = entrada.nextInt();
+			
+		}while(resposta != 0);
 		
-		System.out.println(p1.calculoImc());
 	}
 	
 
